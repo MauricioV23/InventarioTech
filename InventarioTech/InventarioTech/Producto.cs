3d568a5c48bef8be13457e0d93580f7753b64f10
MauricioV23 <126790264+MauricioV23@users.noreply.github.com>
2026-05-07 19:51:04 -0500

    Create Producto.cs

---
+++ InventarioTech/InventarioTech/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InventarioTech
+{
+    internal class Producto
+    {
+        public string Codigo { get; set; }
+
+        public string Nombre { get; set; }
+
+        public double Precio { get; set; }
+
+        public int Cantidad { get; set; }
+    }
+}
